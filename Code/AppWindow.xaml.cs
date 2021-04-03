7a1e09698cea98e739118b17c185c92c833d2ecd
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2021-04-03 13:12:12 +0200

    First commit

---
+++ Code/AppWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ProjektIndywidualny.Code
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
